28bd992817477616dace9931bc80747d416cd4cf
Anish Sahoo <anishsahoo2005@gmail.com>
2024-10-05 13:29:52 -0400

    Add code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {NextUIProvider} from "@nextui-org/react"; 
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <NextUIProvider>
     <App />
+    </NextUIProvider>
   </StrictMode>,
 )
